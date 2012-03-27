923e705b062d96ee8d19ce6782be535392eb01d5
Bastien SIGNORET <bastien@signoret.eu>
2012-03-27 18:57:03 +0200

    Création de la classe HibernateUtile

---
+++ WEB-INF/src/util/HibernateUtil.java
@@ -0,0 +1,45 @@
+package util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
+
+    static {
+        try {
+        	Configuration configuration = new Configuration();
+            configuration.configure().configure("mapping.cfg.xml");
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();        
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+        	System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public static final ThreadLocal<Session> session = new ThreadLocal<Session>();
+    
+    public static Session currentSession() {
+        Session s = (Session) session.get();
+        // Open a new Session, if this Thread has none yet
+        if (s == null) {
+            s = sessionFactory.openSession();
+            session.set(s);
+        }
+        return s;
+    }
+    
+    public static void closeSession() {
+        Session s = (Session) session.get();
+        if (s != null)
+            s.close();
+        session.set(null);
+    }
+}
+
