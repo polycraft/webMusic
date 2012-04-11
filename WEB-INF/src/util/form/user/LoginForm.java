bea66b535d72fd40a185759b54c34e4b67557f26
Bastien SIGNORET <bastien@signoret.eu>
2012-04-12 01:04:59 +0200

    organize import

--- WEB-INF/src/util/form/user/LoginForm.java
+++ WEB-INF/src/util/form/user/LoginForm.java
@@ -1,15 +1,8 @@
 package util.form.user;
 
-import org.hibernate.Session;
-
-import model.Language;
-import model.User;
-
 import util.form.Form;
 import util.validator.BlankValidator;
 import util.validator.ChainValidator;
-import util.validator.LengthMaxValidator;
-import util.validator.SameValidator;
 
 public class LoginForm extends Form {
 
