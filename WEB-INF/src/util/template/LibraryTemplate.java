3202df132dcdfac6ac579737648329bc8f9b3879
FLoCaz <florian.cazalet@fre.fr>
2012-04-13 00:25:49 +0200

    AJout template de librairie

--- WEB-INF/src/util/template/LibraryTemplate.java
+++ WEB-INF/src/util/template/LibraryTemplate.java
@@ -5,13 +5,17 @@ import java.util.Set;
 
 import model.Copy;
 import model.Record;
+import model.Track;
 import model.User;
 
 public class LibraryTemplate {
 
 
+
+	
 	public static String displayFlag(User user ,Record record){
 		
+		if(user== null) return "";
 		boolean alreadyTracked = false;
 		for(Record temp : user.getRecords()){
 			if(temp.equals(record)){
@@ -26,6 +30,14 @@ public class LibraryTemplate {
 			return "<a href=\"flag_record?id="+record.getIdRecord()+"\" title=\"Flag this Record\"><i class=\"icon-flag\"></i></a>";
 	
 		}
+	}
+	
+	public static String recordLink(Record record){
+		return "<a href=\"record_details?id="+record.getIdRecord()+"\">"+record.getTitle()+"</a>";
+	}
+	
+	public static String trackLink(Track track){
+		return "<a href=\"track_details?id="+track.getIdTrack()+"\">"+track.getTitle()+"</a>";
 	}
 	
 }
