a0ca0c021ddd264e97e45e329170949a82b2e4f1
Bastien SIGNORET <bastien@signoret.eu>
2012-04-12 03:18:22 +0200

    Formulaire d'update de profil

---
+++ WEB-INF/src/controller/user/Update.java
@@ -0,0 +1,76 @@
+package controller.user;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.User;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import util.HibernateUtil;
+import util.HttpServlet.HttpServlet;
+import util.form.user.UpdateForm;
+
+@SuppressWarnings("serial")
+public class Update extends HttpServlet {
+
+	private Session sessionHibernate;
+	private Transaction tx;
+
+	private UpdateForm form;
+
+	protected void before(HttpServletRequest request, HttpServletResponse response) {
+		this.form = new UpdateForm();
+		form.setRequest(request);
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
+		// Creation de notre objet Session grace � notre HibernateUtil
+		sessionHibernate = HibernateUtil.currentSession();
+
+		request.setAttribute("languages", sessionHibernate.createQuery("from Language").list());
+
+		HibernateUtil.closeSession();
+
+		request.setAttribute("form", form);
+		RequestDispatcher dispatch = request
+				.getRequestDispatcher("WEB-INF/src/view/user/update.jsp");
+		dispatch.forward(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		
+		if (form.validate()) {System.out.println("good");
+			sessionHibernate = HibernateUtil.currentSession();
+			tx = sessionHibernate.beginTransaction();
+			
+			//Création de l'utilisateur
+			HttpSession session = request.getSession();
+			User user = (User)sessionHibernate.load(User.class, new Integer(session.getAttribute("idUser").toString()));
+			
+			form.fillUser(user,sessionHibernate);
+
+			sessionHibernate.save(user);
+
+			tx.commit();
+
+			HibernateUtil.closeSession();
+
+			RequestDispatcher dispatch = request
+					.getRequestDispatcher("WEB-INF/src/view/user/register_valid.jsp");
+			dispatch.forward(request, response);
+		} else {
+			doGet(request, response);
+		}
+
+	}
+}
